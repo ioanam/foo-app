2ca7803b01a93d1c93b4f95488fa535cf522962d
ioanam <ioanam89@gmail.com>
2021-06-19 16:41:00 +0200

    foo

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+
+const sayHello = () => {
+    fetch('/api/helloWorld')
+        .then(response => {
+            console.log("hello back to you");
+            console.log(response);
+        })
+        .catch(error => {
+            console.error("No hello", error);
+        })
+}
+
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={sayHello}>Hello</button>
+            </header>
+        </div>
+    );
+}
+
+export default App;
